11db9088ec18a29d51b09a7dda1542381ba0a155
SerenEsen <74063743+SerenEsen@users.noreply.github.com>
2022-02-11 16:53:17 +0300

    Add files via upload

---
+++ Guide_App/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace guide_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.name_txt = new System.Windows.Forms.TextBox();
+            this.surname_txt = new System.Windows.Forms.TextBox();
+            this.phone_txt = new System.Windows.Forms.TextBox();
+            this.save_btn = new System.Windows.Forms.Button();
+            this.update_btn = new System.Windows.Forms.Button();
+            this.delete_btn = new System.Windows.Forms.Button();
+            this.persons_dgv = new System.Windows.Forms.DataGridView();
+            this.find_txt = new System.Windows.Forms.TextBox();
+            this.find_btn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ıd_txt = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.persons_dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NAME:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SURNAME:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "PHONE:";
+            // 
+            // name_txt
+            // 
+            this.name_txt.Location = new System.Drawing.Point(108, 83);
+            this.name_txt.Name = "name_txt";
+            this.name_txt.Size = new System.Drawing.Size(125, 27);
+            this.name_txt.TabIndex = 3;
+            this.name_txt.TextChanged += new System.EventHandler(this.name_txt_TextChanged);
+            // 
+            // surname_txt
+            // 
+            this.surname_txt.Location = new System.Drawing.Point(108, 127);
+            this.surname_txt.Name = "surname_txt";
+            this.surname_txt.Size = new System.Drawing.Size(125, 27);
+            this.surname_txt.TabIndex = 4;
+            // 
+            // phone_txt
+            // 
+            this.phone_txt.Location = new System.Drawing.Point(108, 167);
+            this.phone_txt.Name = "phone_txt";
+            this.phone_txt.Size = new System.Drawing.Size(125, 27);
+            this.phone_txt.TabIndex = 5;
+            // 
+            // save_btn
+            // 
+            this.save_btn.Location = new System.Drawing.Point(24, 263);
+            this.save_btn.Name = "save_btn";
+            this.save_btn.Size = new System.Drawing.Size(94, 29);
+            this.save_btn.TabIndex = 6;
+            this.save_btn.Text = "SAVE";
+            this.save_btn.UseVisualStyleBackColor = true;
+            this.save_btn.Click += new System.EventHandler(this.save_btn_Click);
+            // 
+            // update_btn
+            // 
+            this.update_btn.Location = new System.Drawing.Point(24, 315);
+            this.update_btn.Name = "update_btn";
+            this.update_btn.Size = new System.Drawing.Size(94, 29);
+            this.update_btn.TabIndex = 7;
+            this.update_btn.Text = "UPDATE";
+            this.update_btn.UseVisualStyleBackColor = true;
+            this.update_btn.Click += new System.EventHandler(this.update_btn_Click);
+            // 
+            // delete_btn
+            // 
+            this.delete_btn.Location = new System.Drawing.Point(24, 366);
+            this.delete_btn.Name = "delete_btn";
+            this.delete_btn.Size = new System.Drawing.Size(94, 29);
+            this.delete_btn.TabIndex = 8;
+            this.delete_btn.Text = "DELETE";
+            this.delete_btn.UseVisualStyleBackColor = true;
+            this.delete_btn.Click += new System.EventHandler(this.delete_btn_Click);
+            // 
+            // persons_dgv
+            // 
+            this.persons_dgv.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.persons_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.persons_dgv.Location = new System.Drawing.Point(272, 83);
+            this.persons_dgv.Name = "persons_dgv";
+            this.persons_dgv.RowHeadersWidth = 51;
+            this.persons_dgv.RowTemplate.Height = 29;
+            this.persons_dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.persons_dgv.Size = new System.Drawing.Size(483, 332);
+            this.persons_dgv.TabIndex = 9;
+            this.persons_dgv.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.persons_dgv_CellEnter);
+            // 
+            // find_txt
+            // 
+            this.find_txt.Location = new System.Drawing.Point(272, 36);
+            this.find_txt.Name = "find_txt";
+            this.find_txt.Size = new System.Drawing.Size(343, 27);
+            this.find_txt.TabIndex = 10;
+            // 
+            // find_btn
+            // 
+            this.find_btn.Location = new System.Drawing.Point(643, 36);
+            this.find_btn.Name = "find_btn";
+            this.find_btn.Size = new System.Drawing.Size(112, 29);
+            this.find_btn.TabIndex = 11;
+            this.find_btn.Text = "FIND";
+            this.find_btn.UseVisualStyleBackColor = true;
+            this.find_btn.Click += new System.EventHandler(this.find_btn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "ID:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(393, 5);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 13;
+            // 
+            // ıd_txt
+            // 
+            this.ıd_txt.Location = new System.Drawing.Point(108, 45);
+            this.ıd_txt.Name = "ıd_txt";
+            this.ıd_txt.Size = new System.Drawing.Size(125, 27);
+            this.ıd_txt.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(783, 439);
+            this.Controls.Add(this.ıd_txt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.find_btn);
+            this.Controls.Add(this.find_txt);
+            this.Controls.Add(this.persons_dgv);
+            this.Controls.Add(this.delete_btn);
+            this.Controls.Add(this.update_btn);
+            this.Controls.Add(this.save_btn);
+            this.Controls.Add(this.phone_txt);
+            this.Controls.Add(this.surname_txt);
+            this.Controls.Add(this.name_txt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.persons_dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox name_txt;
+        private System.Windows.Forms.TextBox surname_txt;
+        private System.Windows.Forms.TextBox phone_txt;
+        private System.Windows.Forms.Button save_btn;
+        private System.Windows.Forms.Button update_btn;
+        private System.Windows.Forms.Button delete_btn;
+        private System.Windows.Forms.DataGridView persons_dgv;
+        private System.Windows.Forms.TextBox find_txt;
+        private System.Windows.Forms.Button find_btn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox ıd_txt;
+    }
+}
+
